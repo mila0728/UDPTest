84252c2401bcaeb98b36a1a6bda6c535a2cb825d
zhengxiucheng <zhengxiucheng@yihuitek.com>
2020-08-20 11:49:04 +0800

    V1.20

--- app/src/main/java/com/example/udptest/UdpUtil.java
+++ app/src/main/java/com/example/udptest/UdpUtil.java
@@ -30,7 +30,7 @@ public class UdpUtil {
 
 
     public static UdpUtil getInstance() {
-        Log.e("calm","版本———19");
+        Log.e("calm","版本———20");
         if (udpUtil == null) {
             udpUtil = new UdpUtil();
         }
