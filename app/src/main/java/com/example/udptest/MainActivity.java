e046b3b3b8fd7b15f50da0240c969957547680ca
zhengxiucheng <zhengxiucheng@yihuitek.com>
2020-09-07 10:24:16 +0800

    dev2.3

--- app/src/main/java/com/example/udptest/MainActivity.java
+++ app/src/main/java/com/example/udptest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         UdpUtil.getInstance();
         Log.d("calm","main____1.4");
-        Log.d("calm","dev2____2.2");
+        Log.d("calm","dev2____2.3");
         new Thread(new Runnable() {
             @Override
             public void run() {
