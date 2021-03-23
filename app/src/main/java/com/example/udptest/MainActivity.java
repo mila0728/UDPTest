30bcce8d71daf468d7b5e82c41b811d15414ef5b
zhengxiucheng <zhengxiucheng@yihuitek.com>
2021-03-23 10:17:59 +0800

    main  2.19

--- app/src/main/java/com/example/udptest/MainActivity.java
+++ app/src/main/java/com/example/udptest/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         UdpUtil.getInstance();
-        Log.d("calm","修改内容____2.17");
-        Log.d("calm","修改内容____2.17");
-        Log.d("calm","修改内容____2.17");
-        Log.d("calm","测试版本3.10");
+        Log.d("calm","修改内容____2.19");
+        Log.d("calm","修改内容____2.19");
+        Log.d("calm","修改内容____2.19");
+        Log.d("calm","测试版本 2.19");
         new Thread(new Runnable() {
             @Override
             public void run() {
