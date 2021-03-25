db7cb35f1c999c6b2e62d016ffb12cb513f3f2e1
zhengxiucheng <zhengxiucheng@yihuitek.com>
2021-03-25 10:44:38 +0800

    mian 2.34

--- app/src/main/java/com/example/udptest/MainActivity.java
+++ app/src/main/java/com/example/udptest/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("calm","Test修改内容____1.1");
         Log.d("calm","修改内容____2.23");
         Log.d("calm","修改内容____Testxiuga5");
-        Log.d("calm","生产版本2.33");
+        Log.d("calm","生产版本2.34");
         new Thread(new Runnable() {
             @Override
             public void run() {
