e4b19325125c4502ba07eb3af51ecf9680d09986
zhengxiucheng <zhengxiucheng@yihuitek.com>
2021-04-09 11:44:04 +0800

    第一次提交

--- app/src/main/java/com/example/udptest/MainActivity.java
+++ app/src/main/java/com/example/udptest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //提交v1.19
         UdpUtil.getInstance();
         new Thread(new Runnable() {
             @Override
